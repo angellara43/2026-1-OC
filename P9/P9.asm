204bd547756c8fba12fa4ca21818c9683425da56
angellara43 <angel.lara43@uabc.edu.mx>
2026-04-27 23:45:22 +0000

    guardar

--- P9/P9.asm
+++ P9/P9.asm
@@ -5,17 +5,110 @@
 %include "../LIB/pc_iox.inc"
 
 section .data
+msg_input db "Ingrese un vector de tamaño ", 0
+msg_enter db "Ingrese valor (0-9): ", 0
+msg_sum db "Vector suma: ", 0
+msg_dot db "Producto escalar: ", 0
+msg_newline db 10, 0
+N equ 5
 
 section .bss
-arreglo resb 10
+
+vector1 resb 10
+vector2 resb 10
+vector_sum resb 10
 
 section .text
 global _start
-extern getche, puts, putchar
+extern getche, puts, putchar, pHex_b, pHex_dw, clrscr
 
 _start:
+call clrscr
 
-;====================
-; A) Vector tamaño N.
-;====================
+;Leer primer vector
+mov ebx, vector1
+mov ecx, N
+call input_vector
 
+mov edx, msg_newline
+call puts
+
+;Leer segundo vector
+mov ebx, vector2
+mov ecx, N
+call input_vector
+
+mov edx, msg_newline
+call puts
+
+;Calcular suma de vectores
+mov ebx, vector1
+mov edx, vector2
+mov ecx, N
+call sum_vectors
+
+;Desplegar suma
+mov edx, msg_sum
+call puts
+mov ebx, vector_sum
+mov ecx, N
+call output_vector
+
+mov edx, msg_newline
+call puts
+
+;Calcular y desplegar producto escalar
+mov edx, msg_dot
+call puts
+mov ebx, vector1
+mov edx, vector2
+mov ecx, newline
+call dot_product
+
+mov al, 10
+call putchar
+
+;Salir
+mov eax, 1
+xor ebx, ebx
+int 0x80
+
+;======================================
+; A) Procedimiento input_vector
+;======================================
+input_vector:
+push eax
+push ebx
+push ecx
+push esi
+
+xor esi, esi
+
+.input_loop:
+cmp esi, ecx
+jge .input_done
+
+mov edx, msg_enter
+call puts
+
+;Leer caracter
+call getche
+sub al, '0'
+    
+;Validar que esté en rango 0-9
+cmp al, 0
+jb .input_loop
+cmp al, 9
+ja .input_loop
+
+;Almacenar el valor en el vector
+mov byte [ebx + esi], al
+inc esi
+jmp .input_loop
+
+.input_done:
+pop esi
+pop ecx
+pop ebx
+pop eax
+ret
