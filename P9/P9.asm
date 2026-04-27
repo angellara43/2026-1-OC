a97f7e1d6d0f2c1ca9921d0b23740a1ef2b530c6
angellara43 <angel.lara43@uabc.edu.mx>
2026-04-27 23:21:50 +0000

    guardar

---
+++ P9/P9.asm
@@ -0,0 +1,20 @@
+; ================================================
+; Lara Caldera Ángel. 2222625. Práctica de lab #9.
+; ================================================
+
+%include "../LIB/pc_iox.inc"
+
+section .data
+
+section .bss
+arreglo resb 10
+
+section .text
+global _start
+extern getche, puts, putchar
+
+_start:
+
+;================
+; A)
+;================
