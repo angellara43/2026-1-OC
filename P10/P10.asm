9b3c644f2d05b686e22e2615bdb0cf8ccdcb4459
angellara43 <angel.lara43@uabc.edu.mx>
2026-05-04 23:30:12 +0000

    guardar

--- P10/P10.asm
+++ P10/P10.asm
@@ -3,10 +3,21 @@ global suma
 global strlen
 global getBit
 
-Suma: push ebp
+suma: 
+push ebp
 mov ebp, esp
 mov eax, [ebp+8] ;num1
 add eax, [ebp+12] ;num2
+
+strlen:
+push ebp
+mov ebp, esp
+mov edx, [ebp+8]
+xor eax, eax
+
+.ciclo:
+mov cl, byte
+test cl, cl
 
 pop ebp
 ret
